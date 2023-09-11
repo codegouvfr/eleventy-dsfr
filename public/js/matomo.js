b16c039a4cda711295c435be5bbac5c2fd73f90d
Bastien <bzg@gnu.org>
2023-09-11 16:18:55 +0200

    public/js/matomo.js: Update Matomo ID

--- public/js/matomo.js
+++ public/js/matomo.js
@@ -1,14 +1,12 @@
 var _paq = window._paq = window._paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(["setExcludedQueryParams", ["simulationId", "_csrf"]]);
+_paq.push(["setExcludedQueryParams", ["simulationId","_csrf"]]);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
-(function () {
-    var u = "https://stats.data.gouv.fr/";
-    _paq.push(['setTrackerUrl', u + 'matomo.php']);
-    _paq.push(['setSiteId', '95']);
-    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-    g.async = true;
-    g.src = u + 'matomo.js';
-    s.parentNode.insertBefore(g, s);
+(function() {
+  var u="https://stats.data.gouv.fr/";
+  _paq.push(['setTrackerUrl', u+'matomo.php']);
+  _paq.push(['setSiteId', '290']);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
 })();

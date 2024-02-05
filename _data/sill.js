0f6ebe1f034b225e83d33b546e312fb4ac1b44d6
Hélène Jonin <helene.jonin@code.gouv.fr>
2024-02-05 16:10:08 +0100

    Dynamically update cards on home page
    For sill random software and awesome random project

--- _data/sill.js
+++ _data/sill.js
@@ -1,8 +1,8 @@
 const EleventyFetch = require("@11ty/eleventy-fetch");
 
 module.exports = async function () {
-    const URL = "https://code.gouv.fr/data/sill.json";
-    // const URL = "https://code.gouv.fr/sill/api/sill.json"; // To use when SILL is up
+    const URL = "https://code.gouv.fr/sill/api/sill.json"; // To use when SILL is up
+    // const URL = "https://code.gouv.fr/data/sill.json"; // Backup source
 
     let sill = await EleventyFetch(URL, {
         duration: "2w",
---
+++ public/js/home.js
@@ -0,0 +1,39 @@
+(() => {
+    const random = (items) => {
+        return items[Math.floor(Math.random() * items.length)];
+    }
+
+    const logoAltTemplate = (strings, name) => `Logo de ${name}`;
+
+    const sillSoftwareTitleLink = document.querySelector(".sill .fr-card__title a");
+    const sillSoftwareImg = document.querySelector(".sill .fr-card__header img");
+
+    const getSill = async () => {
+        const response = await fetch(`${window.location.origin}/data/sill.json`);
+        return await response.json();
+    }
+
+    getSill().then(softwareList => {
+        const randomSoftware= random(softwareList);
+        sillSoftwareTitleLink.textContent = randomSoftware.name;
+        sillSoftwareTitleLink.href = `https://code.gouv.fr/sill/detail?name=${randomSoftware.name}`;
+        sillSoftwareImg.src = randomSoftware.logoUrl || randomSoftware.comptoirDuLibreSoftware?.logoUrl;
+        sillSoftwareImg.alt = logoAltTemplate`${randomSoftware.name}`;
+    });
+
+    const awesomeProjectTitleLink = document.querySelector(".awesome .fr-card__title a");
+    const awesomeProjectImg = document.querySelector(".awesome .fr-card__header img");
+
+    const getAwesome = async () => {
+        const response = await fetch(`${window.location.origin}/data/awesome.json`);
+        return await response.json();
+    }
+
+    getAwesome().then(awesomeProjects => {
+        const randomProject= random(awesomeProjects);
+        awesomeProjectTitleLink.textContent = randomProject.name;
+        awesomeProjectTitleLink.href = `${window.location.origin}/fr/awesome`;
+        awesomeProjectImg.src = randomProject.logo;
+        awesomeProjectImg.alt = logoAltTemplate`${randomProject.name}`;
+    });
+})();

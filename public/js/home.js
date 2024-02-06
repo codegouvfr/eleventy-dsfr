56dbf624a26e53a7b04d9901f821f17ed2591074
Hélène Jonin <helene.jonin@code.gouv.fr>
2024-02-06 12:01:31 +0100

    Small code cleaning

--- public/js/home.js
+++ public/js/home.js
@@ -1,4 +1,6 @@
 (() => {
+    const dataBaseUrl = `${window.location.origin}/internaldata`;
+
     const random = (items) => {
         return items[Math.floor(Math.random() * items.length)];
     }
@@ -8,15 +10,17 @@
     const sillSoftwareTitleLink = document.querySelector(".sill .fr-card__title a");
     const sillSoftwareImg = document.querySelector(".sill .fr-card__header img");
 
+    const sillDetailUrl = `https://code.gouv.fr/sill/detail`;
+
     const getSill = async () => {
-        const response = await fetch(`${window.location.origin}/internaldata/sill.json`);
+        const response = await fetch(`${dataBaseUrl}/sill.json`);
         return await response.json();
     }
 
     getSill().then(softwareList => {
         const randomSoftware= random(softwareList);
         sillSoftwareTitleLink.textContent = randomSoftware.name;
-        sillSoftwareTitleLink.href = `https://code.gouv.fr/sill/detail?name=${randomSoftware.name}`;
+        sillSoftwareTitleLink.href = `${sillDetailUrl}?name=${randomSoftware.name}`;
         sillSoftwareImg.src = randomSoftware.logoUrl || randomSoftware.comptoirDuLibreSoftware?.logoUrl;
         sillSoftwareImg.alt = logoAltTemplate`${randomSoftware.name}`;
     });
@@ -24,15 +28,17 @@
     const awesomeProjectTitleLink = document.querySelector(".awesome .fr-card__title a");
     const awesomeProjectImg = document.querySelector(".awesome .fr-card__header img");
 
+    const awesomeUrl = `${window.location.origin}/fr/awesome`;
+
     const getAwesome = async () => {
-        const response = await fetch(`${window.location.origin}/internaldata/awesome.json`);
+        const response = await fetch(`${dataBaseUrl}/awesome.json`);
         return await response.json();
     }
 
     getAwesome().then(awesomeProjects => {
         const randomProject= random(awesomeProjects);
         awesomeProjectTitleLink.textContent = randomProject.name;
-        awesomeProjectTitleLink.href = `${window.location.origin}/fr/awesome`;
+        awesomeProjectTitleLink.href = awesomeUrl;
         awesomeProjectImg.src = randomProject.logo;
         awesomeProjectImg.alt = logoAltTemplate`${randomProject.name}`;
     });

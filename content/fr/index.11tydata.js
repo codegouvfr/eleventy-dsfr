93869f344d414dded94f7213c8ee952d2339b521
Bastien Guerry <bzg@gnu.org>
2024-09-07 14:09:37 +0200

    content/fr/index.11tydata.js: Minor UI fixes

--- content/fr/index.11tydata.js
+++ content/fr/index.11tydata.js
@@ -27,14 +27,14 @@ module.exports = {
         {
             title: "Vous êtes une mission de service public et souhaitez référencer vos dépôts de code source ?",
             content: `
-<p>Pour ajouter une forge, un compte d'organisation GitHub ou un groupe GitLab, écrivez à <strong><a href="mailto:contact@code.gouv.fr">contact@code.gouv.fr</a></strong> ou envoyez un correctif sur <a href="https://git.sr.ht/~codegouvfr/codegouvfr-sources/tree/main/item/comptes-organismes-publics.yml" target="_blank">ce dépôt</a>.<p>
-<p><strong>Attention : </strong>Nous ne référençons pas les comptes personnels.</p>
+<p>Pour ajouter une forge, un compte d'organisation GitHub ou un groupe GitLab, écrivez à <a href="mailto:contact@code.gouv.fr">contact@code.gouv.fr</a> ou envoyez un correctif sur <a href="https://git.sr.ht/~codegouvfr/codegouvfr-sources/tree/main/item/comptes-organismes-publics.yml" target="_blank">ce dépôt</a>.<p>
+<p>Attention : Nous ne référençons pas les comptes personnels.</p>
 `
         },
         {
             title: "Vous êtes un BlueHat et souhaitez proposer un atelier ou un article ?",
             content: `
-<p>Écrivez à <strong><a href=\"mailto:contact@code.gouv.fr\">contact@code.gouv.fr</a></strong>.</p>`
+<p>Écrivez à <a href=\"mailto:contact@code.gouv.fr\">contact@code.gouv.fr</a>.</p>`
         }
     ]
 };
